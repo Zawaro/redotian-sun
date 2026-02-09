04687abd243ba761346c15d67817a55e9691cd97
Zawaro <zawaroarts@gmail.com>
2026-02-09 09:07:51 +0200

    refactor: Convert indestation to spaces

--- scripts/ui/camera01.gd
+++ scripts/ui/camera01.gd
@@ -7,12 +7,12 @@ var max_size = 50
 var size_step = 1
 
 func _process(_delta):
-	if Engine.is_editor_hint():
-		self.size = camera_pivot.get("camera_size")
-	if not Engine.is_editor_hint():
-		if Input.is_action_just_pressed("zoom_in"):
-			var camera_size = self.size - size_step
-			self.size = clamp(camera_size, min_size, max_size)
-		if Input.is_action_just_pressed("zoom_out"):
-			var camera_size = self.size + size_step
-			self.size = clamp(camera_size, min_size, max_size)
+    if Engine.is_editor_hint():
+        self.size = camera_pivot.get("camera_size")
+    if not Engine.is_editor_hint():
+        if Input.is_action_just_pressed("zoom_in"):
+            var camera_size = self.size - size_step
+            self.size = clamp(camera_size, min_size, max_size)
+        if Input.is_action_just_pressed("zoom_out"):
+            var camera_size = self.size + size_step
+            self.size = clamp(camera_size, min_size, max_size)
